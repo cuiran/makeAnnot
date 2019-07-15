8b4391795a98956447fc9dec1bb315aca6f74c71
Ran Cui <cranmail@gmail.com>
2019-07-15 13:32:07 -0400

    attempting to fix bug in paste command

--- concat_annots.sh
+++ concat_annots.sh
@@ -8,8 +8,11 @@ DIR=$5
 OUT=$6
 
 command=paste
-for i in ${DIR}Roadmap.{$S..$E}.$CHR.annot.gz; do
-    command="$command <(gzip -cd $i | cut -f5)"
+ 
+for i in ${DIR}Roadmap.{$S..$E}.$CHR.annot.gz
+    do command="$command <(gzip -cd $i | cut -f5)"
 done
+
 eval $command > ${OUT}${NAME}.concatenated.$CHR.annot
+
 echo ${OUT}${NAME}.concatenated.$CHR.annot
